8f7feb74399b544413dd42e87de06a529f477844
JYuan <yuanjie1019sjtu@qq.com>
2018-09-17 18:23:59 +0800

    add constant file

---
+++ constant.py
@@ -0,0 +1,7 @@
+futures_type = ['A.DCE', 'AG.SHF', 'AL.SHF', 'AP.CZC', 'AU.SHF', 'B.DCE', 'BB.DCE', 'BU.SHF', 'C.DCE', 'CF.CZC',
+                'CS.DCE', 'CU.SHF', 'CY.CZC', 'FB.DCE', 'FG.CZC', 'FU.SHF', 'HC.SHF', 'I.DCE', 'J.DCE', 'JD.DCE',
+                'JM.DCE', 'JR.CZC', 'L.DCE', 'LR.CZC', 'M.DCE', 'MA.CZC', 'NI.SHF', 'OI.CZC', 'P.DCE', 'PB.SHF',
+                'PM.CZC', 'PP.DCE', 'RB.SHF', 'RI.CZC', 'RM.CZC', 'RS.CZC', 'RU.SHF', 'SC.INE', 'SF.CZC',
+                'SM.CZC', 'SN.SHF', 'SR.CZC', 'TA.CZC', 'V.DCE', 'WH.CZC', 'Y.DCE', 'ZC.CZC', 'ZN.SHF', 'WR.SHF',
+                'B.IPE', 'V.DCE']
+
