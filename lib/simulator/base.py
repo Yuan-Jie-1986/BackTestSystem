c2766ec29adbcbb67fbace09acbc66bd9e44104b
JYuan <yuanjie1019sjtu@qq.com>
2019-01-07 18:28:47 +0800

    修正了一些bug

--- lib/simulator/base.py
+++ lib/simulator/base.py
@@ -337,7 +337,10 @@ class BacktestSys(object):
         dt_error = [datetime(2014, 12, 2), datetime(2014, 12, 15), datetime(2014, 12, 19), datetime(2014, 12, 22),
                     datetime(2014, 12, 23)]
         for d in dt_error:
-            self.data['BU.SHF']['OPEN'][self.dt == d] = self.data['BU.SHF']['CLOSE'][self.dt == d]
+            try:
+                self.data['BU.SHF']['OPEN'][self.dt == d] = self.data['BU.SHF']['CLOSE'][self.dt == d]
+            except KeyError, e:
+                pass
 
 
     def strategy(self):
