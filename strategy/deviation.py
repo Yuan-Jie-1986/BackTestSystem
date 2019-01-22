c4ab9302b30d12fa4e466c09bbeb89b3a7a1715e
JYuan <yuanjie1019sjtu@qq.com>
2019-01-23 00:16:17 +0800

    偏离

--- strategy/deviation.py
+++ strategy/deviation.py
@@ -11,6 +11,7 @@ class Deviation(BacktestSys):
     def strategy(self):
         wgtsDict = {}
         for k in self.data:
+            wgtsDict[k] = np.zeros_like(self.dt)
             cls = self.data[k]['CLOSE']
             cls_df = pd.DataFrame({'CLOSE': cls}, index=self.dt)
             cls_df['mon_day'] = [d.strftime('%m-%d') for d in self.dt]
@@ -31,13 +32,39 @@ class Deviation(BacktestSys):
                     except ValueError:
                         temp_value.drop(index=d, inplace=True)
 
-                temp = pd.DataFrame({'CLOSE': temp_value.values}, index=temp_idx)
+                temp = pd.DataFrame({'CLOSE_F': temp_value.values}, index=temp_idx)
                 future_df = pd.concat([future_df, temp], axis=0)
-            future_df = future_df.shift(periods=-10, axis=0)
+            future_df.fillna(method='ffill', inplace=True, limit=3)
+            future_df = future_df.rolling(window=20).mean()
+            future_df = future_df.shift(periods=-20, axis=0)
 
-            print future_df
+            cls_df = cls_df.join(future_df, how='left')
+            cls_df['rtn'] = cls_df['CLOSE'] / cls_df['CLOSE_F'] - 1
+            cls_df['rtn_std'] = cls_df['rtn'].rolling(window=20).std()
+            cls_df['rtn_mean'] = cls_df['rtn'].rolling(window=20).mean()
+            cls_df['rtn_standard'] = (cls_df['rtn'] - cls_df['rtn_mean']) / cls_df['rtn_std']
+            rtn_standard = cls_df['rtn_standard'].values.flatten()
+
+            for i in np.arange(1, len(self.dt)):
+                if rtn_standard[i] >= 2.5 and wgtsDict[k][i-1] == 0:
+                    wgtsDict[k][i] = 1
+                elif rtn_standard[i] <= -2.5 and wgtsDict[k][i-1] == 0:
+                    wgtsDict[k][i] = -1
+                elif rtn_standard[i] < 0 and wgtsDict[k][i-1] == -1:
+                    wgtsDict[k][i] = 0
+                elif rtn_standard[i] > 0 and wgtsDict[k][i-1] == 1:
+                    wgtsDict[k][i] = 0
+                else:
+                    wgtsDict[k][i] = wgtsDict[k][i-1]
+
+        return wgtsDict
 
 
 if __name__ == '__main__':
     a = Deviation()
-    a.strategy()
+    wgt = a.strategy()
+    wgt = a.wgtsStandardization(wgt)
+    wgt = a.wgtsProcess(wgt)
+
+    a.displayResult(wgt)
+
